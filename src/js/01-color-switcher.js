1ef48a94e39d6f79c096851ccb4a6aefea3e5d11
nayanzina@gmail.com <wwwtima.cherin@gmail.com>
2022-12-03 18:48:17 +0200

    hw-09 task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+let timer;
+
+function onStartBtnClick() {
+    refs.startBtn.setAttribute('disabled', 'disabled');
+    refs.stopBtn.removeAttribute('disabled');
+
+    timer = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000) 
+};
+
+function onStopBtnClick() {
+    if (refs.startBtn.getAttribute('disabled')) {
+        refs.stopBtn.setAttribute('disabled', 'disabled');
+        refs.startBtn.removeAttribute('disabled');
+    }
+    clearInterval(timer);
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
