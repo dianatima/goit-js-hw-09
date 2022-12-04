d201adba3afc5ff4e47e02c4aafc53ebf2689d2f
nayanzina@gmail.com <wwwtima.cherin@gmail.com>
2022-12-04 03:29:58 +0200

    hw-09

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+let position = 0;
+let formDatadelay = 0;
+let step = 0;
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const formElements = e.currentTarget.elements;
+  delay = Number(formElements.delay.value);
+  step = Number(formElements.step.value);
+  amount = Number(formElements.amount.value);
+
+  let promiseDelay = delay;
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, promiseDelay);
+    promiseDelay += step;
   }
+}
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  })
+    .then(x => Notiflix.Notify.success(x))
+    .catch(error => Notiflix.Notify.failure(error));
 }
